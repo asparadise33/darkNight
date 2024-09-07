83bae75be49adcbdfd47966454b8765a43b8652b
asparadise33 <abby.paradise@gmail.com>
2024-09-07 12:49:42 -0500

    updated several files, cards are on the DOM

--- components/jobCard.js
+++ components/jobCard.js
@@ -1,45 +1,36 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-// import Link from 'next/link';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
 
-// function JobCard({ jobObj, onUpdate }) {
-//   const deleteThisJob = () => {
-//     if (window.confirm(`Delete ${jobObj.title}?`)) {
-//       deleteJob(jobObj.firebaseKey).then(() => onUpdate());
-//     }
-//   };
+function JobCard({ jobObj }) {
+  return (
+    <Card className="card" style={{ width: '18rem', margin: '10px', border: '1px solid' }}>
+      <Card.Title style={{ textAlign: 'center', paddingTop: '10px' }}>{jobObj.job_name}</Card.Title>
+      <Card.Body>
+        <p className="card-text bold">{jobObj.company_name}</p>
+        <Link href="/view" passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        <Link href="/edit" passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+}
 
-//   return (
-//     <Card style={{ width: '18rem', margin: '10px' }}>
-//       <Card.Body>
-//         <Card.Title>{jobObj.first_name} {jobObj.last_name}</Card.Title>
-//         <p>{jobObj.email}</p>
-//         <p className="card-text bold">{jobObj.favorite && <span>Favorite<br /></span> } ${jobObj.favorite}</p>
-//         <Link href={`/author/${jobObj.firebaseKey}`} passHref>
-//           <Button variant="primary" className="m-2">VIEW</Button>
-//         </Link>
-//         <Link href={`/author/edit/${jobObj.firebaseKey}`} passHref>
-//           <Button variant="info">EDIT</Button> */
-//         </Link>
-//         <Button variant="danger" onClick={deleteThisJob} className="m-2">
-//           DELETE
-//         </Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
+JobCard.propTypes = {
+  jobObj: PropTypes.shape({
+    job_name: PropTypes.string,
+    job_type: PropTypes.string,
+    boardName: PropTypes.string,
+    company_name: PropTypes.string,
+    description: PropTypes.string,
+    notes: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+};
 
-// JobCard.propTypes = {
-//   jobObj: PropTypes.shape({
-//     first_name: PropTypes.string,
-//     last_name: PropTypes.string,
-//     favorite: PropTypes.bool,
-//     email: PropTypes.string,
-//     firebaseKey: PropTypes.string,
-//   }).isRequired,
-//   onUpdate: PropTypes.func.isRequired,
-// };
-
-// export default JobCard;
+export default JobCard;
---
+++ pages/view/[firebaseKey].js
@@ -0,0 +1,34 @@
+// /* eslint-disable @next/next/no-img-element */
+// import React, { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { viewAuthorDetails } from '../../api/mergedData';
+// import BookCard from '../../components/BookCard';
+
+// export default function ViewAuthor() {
+//   const [authorDetails, setAuthorDetails] = useState({});
+//   const router = useRouter();
+//   const { firebaseKey } = router.query;
+
+//   useEffect(() => {
+//     viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+//   }, [firebaseKey]);
+
+//   return (
+//     <div className="mt-5 d-flex flex-wrap">
+//       <div className="text-white ms-5 details">
+//         <h5>
+//           By {authorDetails.first_name} {authorDetails.last_name}
+//           {authorDetails.favorite ? ' 🤍' : ''}
+//         </h5>
+//         Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
+//         <p>{authorDetails.description || ''}</p>
+//         <hr />
+//         <div className="d-flex flex-wrap">
+//           {authorDetails.books?.map((book) => (
+//             <BookCard key={book.firebaseKey} bookObj={book} onUpdate={viewAuthorDetails} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
